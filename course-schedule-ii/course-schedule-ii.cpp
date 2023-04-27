0b68f75ba5b0ef647ddaca3f7b090cce9bfcdd13
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-04-28 03:38:48 +0530

    Time: 19 ms (85.30%) | Memory: 14.2 MB (32.56%) - LeetSync

---
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        // Initialize the adjacency list and the in-degree vector
+        vector<vector<int>> adjList(numCourses);
+        vector<int> inDegree(numCourses);
+
+        for (auto p : prerequisites) 
+        {
+            int u = p[1], v = p[0];
+            adjList[u].push_back(v);
+
+            inDegree[v]++;
+        }
+        
+        // Initialize the queue with all nodes having zero in-degree
+        queue<int> q;
+        
+        for (int i = 0; i < numCourses; i++) 
+        {
+            if (inDegree[i] == 0) 
+            {
+                q.push(i);
+            }
+        }
+        
+        // Perform the Kahn's algorithm
+        vector<int> res;
+
+        while (!q.empty()) 
+        {
+            int u = q.front();
+            q.pop();
+
+            res.push_back(u);
+
+            for (int v : adjList[u]) 
+            {
+                inDegree[v]--;
+
+                if (inDegree[v] == 0) 
+                {
+                    q.push(v);
+                }
+            }
+        }
+        
+        // Check if all nodes have been visited
+        if (res.size() != numCourses) 
+        {
+            return {};
+        }
+        
+        // Return the result
+        return res;
+    }
+};
