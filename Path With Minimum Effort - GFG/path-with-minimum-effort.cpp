c1283ce455d0ec48f4e766b64786b6d7b8b28ae4
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-05-05 00:18:38 +0530

    Added solution - LeetHub

---
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int MinimumEffort(vector<vector<int>>& height) 
+    {
+        // Code here
+        int n = height.size();
+        int m = height[0].size();
+
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
+
+        priority_queue< pair<int, pair<int, int>> ,
+                        vector<pair<int, pair<int, int>>> ,
+                        greater<pair<int, pair<int, int>>> >pq;
+        pq.push({0,{0,0}});
+
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+
+        while(!pq.empty())
+        {
+            int dis=pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+
+            pq.pop();
+
+            if(r==n-1 && c==m-1)
+                return dis;
+
+            for(int i=0;i<4;i++)
+            {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m)
+                {
+                    int effort = max(dis,abs(height[r][c]-
+                                             height[nr][nc]));
+                    
+                    if(effort < dist[nr][nc])
+                    {
+                        dist[nr][nc]=effort;
+                        pq.push({effort,{nr,nc}});
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n,m; cin>>n>>m;
+        vector<vector<int>> heights;
+       
+        for(int i=0; i<n; ++i){
+            vector<int> temp;
+            for(int j=0; j<m; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            heights.push_back(temp);
+        }
+       
+        Solution obj;
+        cout<<obj.MinimumEffort(heights)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
