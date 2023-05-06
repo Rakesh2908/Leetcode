1eeb058140c0145dd4b6ea388b7bb8e9bd640a8b
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-05-06 15:42:51 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,48 +3,121 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet
+{
+    vector<int>parent,rank,size;
+    
+    public:
+    
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        size.resize(n+1);
+        parent.resize(n+1);
+        
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int findUPar(int node)
+    {
+        if(node == parent[node])
+            return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    
+    void UnionByRank(int u,int v)
+    {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        
+        if(pu==pv)  
+            return;
+            
+        if(rank[pu] < rank[pv])
+        {
+            parent[pu]=pv;
+        }
+        
+        else if(rank[pv] < rank[pu])
+        {
+            parent[pv]=pu;
+        }
+        
+        else
+        {
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+    
+    void UnionBySize(int u,int v)
+    {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        
+        if(pu==pv)  
+            return;
+            
+        if(size[pu] < size[pv])
+        {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        
+        else
+        {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
+    int spanningTree(int n, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int, int>,
-		               vector<pair<int, int> >, 
-		               greater<pair<int, int>>> pq;
-		vector<int> vis(V,0);
-		
-		int sum=0;
-		
-		pq.push({0,0});
-		
-		while(!pq.empty())
-		{
-		    int wt = pq.top().first;
-		    int node = pq.top().second;
-		    
-		    pq.pop();
-		    
-		    if(vis[node])
-		        continue;
-		        
-		    vis[node]=1;
-		    sum += wt;
-		    
-		    for(auto it: adj[node])
-		    {
-		        int v = it[0];
-		        int edwt = it[1];
-		        
-		        if(!vis[v])
-		        {
-		            pq.push({edwt,v});
-		        }
-		    }
-		}
-		
-		return sum;
+        vector<pair<int,pair<int,int>>> edges;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                int u = i;
+                int v = it[0];
+                int wt = it[1];
+                
+                edges.push_back({wt,{u,v}});
+            }
+        }
+        
+        sort(edges.begin(),edges.end());
+        
+        DisjointSet ds(n);
+        
+        int sum=0;
+        
+        for(auto it: edges)
+        {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findUPar(u) != ds.findUPar(v))
+            {
+                sum += wt;
+                ds.UnionBySize(u,v);
+            }
+        }
+        
+        return sum;
     }
 };
 
