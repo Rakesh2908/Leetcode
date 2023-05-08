84f3bfa46c2a78b77ac67b3c4e5f54c9f57838cd
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-05-08 06:30:48 +0530

    Time: 237 ms (35.57%) | Memory: 60.2 MB (54.10%) - LeetSync

--- sort-list/sort-list.cpp
+++ sort-list/sort-list.cpp
@@ -10,32 +10,33 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) 
-    {
-        if (head == nullptr || head->next == nullptr) 
-        {
+    ListNode* sortList(ListNode* head) {
+        // base case: empty or single node list is already sorted
+        if (head == nullptr || head->next == nullptr) {
             return head;
         }
         
-        vector<int> ans;
-        ListNode* curr = head;
-        while (curr != nullptr) 
-        {
-            ans.push_back(curr->val);
-            curr = curr->next;
+        // create a vector to store the values in the list
+        vector<int> values;
+        while (head != nullptr) {
+            values.push_back(head->val);
+            head = head->next;
         }
         
-        sort(begin(ans), end(ans));
+        // sort the values in ascending order
+        sort(values.begin(), values.end());
         
-        ListNode* res = new ListNode(0);
-        ListNode* dummy = res;
-        for (int answ : ans) 
-        {
-            dummy->next = new ListNode(answ);
-            dummy = dummy->next;
+        // create a dummy head node for the sorted list
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        
+        // create a new node for each sorted value and attach it to the sorted list
+        for (int value : values) {
+            current->next = new ListNode(value);
+            current = current->next;
         }
-        dummy->next = nullptr;
+        current->next = nullptr; // set the end of the sorted list to null
         
-        return res->next;
+        return dummy->next; // skip the dummy head node and return the actual head of the sorted list
     }
 };
