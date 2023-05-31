678066692e29f8e3b74279a61b28a09b324a6121
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-05-31 19:23:25 +0530

    Time: 40 ms (42.61%) | Memory: 57.9 MB (39.66%) - LeetSync

--- recover-binary-search-tree/recover-binary-search-tree.cpp
+++ recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,42 +1,35 @@
-class Solution 
-{
+class Solution {
 public:
     TreeNode* firstElement = nullptr;
     TreeNode* secondElement = nullptr;
-    TreeNode* prevElement = nullptr;
+    TreeNode* prevElement = new TreeNode(INT_MIN);
 
     void recoverTree(TreeNode* root) 
     {
-        findSwappedElements(root);
-        swapValues(firstElement, secondElement);
+        traverse(root);
+        swap(firstElement->val,secondElement->val);
     }
 
 private:
-    void findSwappedElements(TreeNode* root) 
+    void traverse(TreeNode* root) 
     {
         if (root == nullptr)
             return;
-
-        findSwappedElements(root->left);
-
-        if (prevElement != nullptr && prevElement->val >= root->val) 
+            
+        traverse(root->left);
+        
+        if (firstElement == nullptr && prevElement->val > root->val) 
         {
-            if (firstElement == nullptr) 
-            {
-                firstElement = prevElement;
-            }
-            secondElement = root;
+            firstElement = prevElement;
         }
+
+        if (firstElement != nullptr && prevElement->val > root->val) 
+        {
+            secondElement = root;
+        } 
 
         prevElement = root;
 
-        findSwappedElements(root->right);
-    }
-
-    void swapValues(TreeNode* node1, TreeNode* node2) 
-    {
-        int temp = node1->val;
-        node1->val = node2->val;
-        node2->val = temp;
+        traverse(root->right);
     }
 };
