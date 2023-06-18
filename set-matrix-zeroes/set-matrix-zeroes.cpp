74ebdc451e4ccfbca4fe333456e22498390d8820
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-06-18 11:44:31 +0530

    Time: 25 ms (9.01%) | Memory: 13.3 MB (52.50%) - LeetSync

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,49 +2,31 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int nr = matrix.size();
+        int nc = matrix[0].size();
 
-        int i,j,i1,j1,k;
+        vector<int> r(nr,1);
+        vector<int> c(nc,1);
 
-        vector<vector<int>> temp(m,vector<int>(n));
-
-        for(i=0;i<m;i++)
+        for(int i=0;i<nr;i++)
         {
-            for(j=0;j<n;j++)
-            {
-                temp[i][j]=matrix[i][j];
-            }
-        }
-
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
+            for(int j=0;j<nc;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    temp[i][j]=0;
-
-                    i1=i,j1=j;
-
-                    for(k=0;k<n;k++)
-                    {
-                        temp[i1][k]=0;
-                    }
-
-                    for(k=0;k<m;k++)
-                    {
-                        temp[k][j1]=0;
-                    }
+                    r[i]=0;
+                    c[j]=0;
                 }
             }
         }
-
-        for(i=0;i<m;i++)
+        for(int i=0;i<nr;i++)
         {
-            for(j=0;j<n;j++)
+            for(int j=0;j<nc;j++)
             {
-                matrix[i][j]=temp[i][j];
+                if(r[i]==0 || c[j]==0)
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
     }
