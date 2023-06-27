5ec065b4cbd48b193676e239f8a98e80c94dc06a
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-06-27 19:10:54 +0530

    Time: 5 ms (76.78%) | Memory: 7.8 MB (88.75%) - LeetSync

---
+++ counting-bits/counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> result(n + 1, 0);
+        for (int i = 1; i <= n; i++) 
+        {
+            // Use the previously computed count for i/2
+            result[i] = result[i / 2] + (i % 2);
+        }
+        return result;
+    }
+};
