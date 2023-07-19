f74d584f2d4a00dab7b669ab794ef3e6fd687d78
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-07-19 13:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (88.45%) - LeetSync

--- 1033-broken-calculator/broken-calculator.cpp
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int brokenCalc(int startValue, int target) 
-    {
-        int ans=0;
+    int brokenCalc(int X, int Y) {
+        int operations = 0;
 
-        if(startValue==target)  return 0;
-
-        //instead of going from startvalue to target we do it reverse
-        while(startValue < target)
-        {
-            if(target%2==1)
-            {
-                ans++;
-                target++;
+        while (Y > X) {
+            // If Y is greater than X, we have two options:
+            // 1. If Y is odd, we have to perform a decrement operation to make it even.
+            // 2. If Y is even, we can just perform a double operation.
+            // We always choose the operation that reduces Y to the nearest even number
+            // since using double will reduce the difference between X and Y faster.
+            if (Y % 2 == 1) {
+                Y++; // Increment Y to make it even.
+                operations++; // Perform the decrement operation.
             }
 
-            target /= 2;
-            ans++;
+            Y /= 2; // Perform the double operation.
+            operations++; // Increment the number of operations performed.
         }
 
-        return ans + (startValue-target);
+        // At this point, Y is not greater than X, so we just need to perform X - Y decrement operations
+        // to reach the target value X from Y.
+        return operations + (X - Y);
     }
 };
