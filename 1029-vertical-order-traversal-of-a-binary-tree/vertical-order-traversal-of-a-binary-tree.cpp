5909fe28755f2e0530bb382f96b10765a26b22c5
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-07-23 22:47:25 +0530

    Time: 7 ms (51.78%) | Memory: 13.5 MB (45.83%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> vertical; // x, y, set of nodes
-        queue<pair<TreeNode*, pair<int, int>>> q; // node, x, y
-        q.push({root, {0, 0}});
+    vector<vector<int>> verticalTraversal(TreeNode* root) 
+    {
+        map<int, map<int, multiset<int>>> nodes; // x, y, set of nodes
+        queue<pair<TreeNode*, pair<int, int>>> todo; // node, x, y
+        todo.push({root, {0, 0}}); // push root node with (x, y) position
 
-        while (!q.empty()) {
-            auto p = q.front();
-            TreeNode* node = p.first;
-            int x = p.second.first;
-            int y = p.second.second;
-            q.pop();
+        while (!todo.empty()) 
+        { // traverse the tree
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* node = p.first; // current node
+            int x = p.second.first, y = p.second.second; // coordinates
 
-            vertical[x][y].insert(node->val);
+            nodes[x][y].insert(node->val); // store node value based on (x, y) position
 
-            if (node->left) {
-                q.push({node->left, {x - 1, y + 1}});
+            if (node->left) 
+            { // if left child exists
+                todo.push({node->left, {x - 1, y + 1}}); // enqueue left child with updated position
             }
 
-            if (node->right) {
-                q.push({node->right, {x + 1, y + 1}});
+            if (node->right) 
+            { // if right child exists
+                todo.push({node->right, {x + 1, y + 1}}); // enqueue right child with updated position
             }
         }
 
-        vector<vector<int>> ans;
+        vector<vector<int>> ans; // result vector
 
-        for (const auto& x_pair : vertical) 
-        {
-            vector<int> col;
+        for (auto p : nodes) 
+        { // iterate through the stored nodes
+            vector<int> col; // column vector
 
-            for (const auto& y_pair : x_pair.second) 
-            {
-                col.insert(col.end(), 
-                            y_pair.second.begin(), y_pair.second.end());
+            for (auto q : p.second) 
+            { // iterate through nodes with same x position
+                col.insert(col.end(), q.second.begin(), q.second.end()); // insert nodes in the column
             }
 
-            ans.push_back(col);
+            ans.push_back(col); // push the column to the result
         }
 
-        return ans;
+        return ans; // return the vertical order traversal
     }
 };
