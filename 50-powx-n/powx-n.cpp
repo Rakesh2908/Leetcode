91708924e83344cbf76ee70d3ecfc9ea9ca2fb4b
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-07-24 11:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 5.8 MB (99.91%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    double myPow(double x, int n) 
-    {
-        if (n == 0) 
-        {
-            return 1.0;   // Base case: x^0 = 1
+    double binaryExp(double x, long long n) {
+        // Base case, to stop recursive calls.
+        if (n == 0) {
+            return 1;
         }
-        
-        // Recursive call to compute x^(n/2)
-        double half = myPow(x, n / 2);   
-        
-        // Check if n is even or odd
-        if (n % 2 == 0) 
-        {
-            return half * half;   // If n is even, x^n = (x^(n/2))^2
-        } 
-        
-        else if (n > 0) 
-        {
-            // If n is odd and positive, x^n = x * (x^(n/2))^2
-            return x * half * half;   
-        } 
-        
-        else 
-        {
-            // If n is odd and negative, x^n = (1/x) * (x^(n/2))^2
-            return (1 / x) * half * half;   
+       
+        // Handle case where, n < 0.
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -1 * n);
         }
+       
+        // Perform Binary Exponentiation.
+        // If 'n' is odd we perform Binary Exponentiation on 'n - 1' and multiply result with 'x'.
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        }
+        // Otherwise we calculate result by performing Binary Exponentiation on 'n'.
+        else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        return binaryExp(x, (long long) n);
     }
 };
