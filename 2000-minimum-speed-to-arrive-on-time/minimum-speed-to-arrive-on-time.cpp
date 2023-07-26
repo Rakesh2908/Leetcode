5ec8d611d0a2c78dda7f3ff77bf6c0b527ccc004
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-07-26 22:51:31 +0530

    Time: 360 ms (45.04%) | Memory: 101.4 MB (67.19%) - LeetSync

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    double timeRequired(vector<int>& dist, int speed) 
+    double timeReq(int speed, vector<int>&dist)
     {
         double time = 0.0;
 
-        for (int i = 0 ; i < dist.size(); i++) 
+        for(int i=0;i<dist.size();i++)
         {
-            double t = (double) dist[i] / (double) speed;
-            // Round off to the next integer, if not the last ride.
-            time += (i == dist.size() - 1 ? t : ceil(t));
+            double t = (double)dist[i]/speed;
+
+            time += (i==dist.size()-1)?t:ceil(t);
         }
         return time;
     }
-    
+
     int minSpeedOnTime(vector<int>& dist, double hour) 
     {
-        int left = 1;
-        int right = 1e7;
-        int minSpeed = -1;
-        
-        while (left <= right) 
+        int mini=-1;
+        int left=1;
+        int right=(int)1e7;
+
+        while(left <= right)
         {
-            int mid = (left + right) / 2;
-            
-            // Move to the left half.
-            if (timeRequired(dist, mid) <= hour) 
+            int mid = left + (right-left)/2;
+
+            if(timeReq(mid,dist) <= hour)
             {
-                minSpeed = mid;
-                right = mid - 1;
-            } 
-            
-            else 
+                mini = mid;
+                right=mid-1;
+            }
+
+            else
             {
-                // Move to the right half.
-                left = mid + 1;
+                left=mid+1;
             }
         }
-        return minSpeed;
+
+        return mini;
     }
 };
