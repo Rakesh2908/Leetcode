ed0c499bb0cd7be427e44cde067eaa247d497b89
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-08-14 18:57:05 +0530

    Time: 317 ms (41.27%) | Memory: 85.5 MB (62.04%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    int f(int i,int j,vector<int>& nums1, vector<int>& nums2, 
-          vector<vector<int>>&dp)
-    {
-        if(i<0 || j<0)  return 0;
-
-        if(dp[i][j] != -1)  return dp[i][j];
-
-       
-        
-        int take=0;
-        if(nums1[i]==nums2[j])
-        {
-            take = 1+f(i-1,j-1,nums1,nums2,dp);
+    int f(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0) {
+            return 0;
         }
 
-        return dp[i][j]=take;
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int take = 0;
+        if (nums1[i] == nums2[j]) {
+            take = 1 + f(i - 1, j - 1, nums1, nums2, dp);
+        }
+
+        return dp[i][j] = take;
     }
 
-    int findLength(vector<int>& nums1, vector<int>& nums2) 
-    {
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
 
-        int maxi=0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++)
+        int maxi = 0;
+        for (int i = 0; i < n; i++) 
         {
-            for(int j=1;j<=m;j++)
+            for (int j = 0; j < m; j++) 
             {
-                if(nums1[i-1]==nums2[j-1])
-                {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    maxi = max(maxi,dp[i][j]);
-                }
-
-                else
-                {
-                    dp[i][j]=0;
-                }
+                int length = f(i, j, nums1, nums2, dp);
+                maxi = max(maxi, length);
             }
         }
+
         return maxi;
     }
 };
