add1bb8aec336834ceaf7d9d146cbbf65e94cffb
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-08-21 10:58:42 +0530

    Time: 2 ms (54.06%) | Memory: 7.5 MB (28.97%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -1,35 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) 
-    {
-        if(!head || !head->next) 
-            return head; 
-		
-        ListNode* temp = new ListNode();
-        
-        ListNode* prev=temp;
-        ListNode* curr=head;
-        
-        while(curr && curr->next)
-        {
-            prev->next = curr->next;
-            curr->next = prev->next->next;
-            prev->next->next = curr;
-            
-            prev = curr;
-            curr = curr->next;
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next)  // If the list is empty or has only one node, no swaps needed
+            return head;
+
+        ListNode* temp = new ListNode();  // Create a temporary node to serve as the dummy head
+
+        ListNode* prev = temp;  // Initialize prev pointer to the temporary node
+        ListNode* curr = head;  // Initialize curr pointer to the head of the list
+
+        while (curr && curr->next) {
+            prev->next = curr->next;          // Update prev's next to point to curr's next
+            curr->next = prev->next->next;    // Update curr's next to skip the next node
+            prev->next->next = curr;          // Update the next of the node after prev to point to curr
+
+            prev = curr;  // Move prev to curr position (prev now becomes the last node in the swapped pair)
+            curr = curr->next;  // Move curr to the next pair's starting position
         }
-        
-        return temp->next;
+
+        return temp->next;  // Return the new head of the linked list after swapping
     }
 };
