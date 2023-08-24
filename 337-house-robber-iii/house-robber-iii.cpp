c0ab03360db9575e307b9be9b14dffb51c136195
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-08-24 23:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (67.66%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -9,24 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
-    vector<int> f(TreeNode* root)
+    pair<int, int> fun(TreeNode* root)
     {
-        if(!root)   return {0,0};
+        if(root == NULL)
+            return {0, 0};
+        
+        pair<int, int> l = fun(root->left);
+        pair<int, int> r = fun(root->right);
 
-        vector<int> l = f(root->left);
-        vector<int> r = f(root->right);
+        int take = root->val + l.second + r.second;
+        int notTake = max(l.first, l.second) + max(r.first, r.second);
 
-        int rob = root->val + l[1] + r[1];
-        int notRob = max(l[0],l[1]) + max(r[0],r[1]);
-
-        return {rob,notRob};
+        return {take, notTake};
     }
 
     int rob(TreeNode* root) 
     {
-        vector<int>ans=f(root);
-        return max(ans[0],ans[1]);
+        pair<int, int> ans = fun(root);
+
+        return max(ans.first, ans.second);
     }
 };
