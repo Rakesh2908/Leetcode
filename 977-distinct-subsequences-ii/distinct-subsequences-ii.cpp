3ba6c4bbdd0d640c7e1fc8852ddfc0ef4099b7c7
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-10-02 07:46:52 +0530

    Time: 4 ms (68.06%) | Memory: 6.7 MB (79.72%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int distinctSubseqII(string S) 
+    {
+        long endsWith[26] = {};
+        long mod = 1e9 + 7;
+        for (char c : S)
+            endsWith[c - 'a'] = accumulate(begin(endsWith), 
+                                           end(endsWith), 1L) % mod;
+                                           
+        return accumulate(begin(endsWith), end(endsWith), 0L) % mod;
+    }
+};
