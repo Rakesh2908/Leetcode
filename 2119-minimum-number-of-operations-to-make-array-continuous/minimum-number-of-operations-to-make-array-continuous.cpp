908987daba0d7da52d58d104fd8d3b2ddec34a07
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-10-10 14:09:30 +0530

    Time: 128 ms (98.54%) | Memory: 64.7 MB (67.78%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -2,29 +2,12 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) 
     {
-        int n = nums.size();
-        int ans = n;
-        
-        // std::set automatically sorts
-        set<int> unique(nums.begin(), nums.end());
-        vector<int> newNums;
-        for (int num : unique) 
-        {
-            newNums.push_back(num);
+        int n = nums.size(), i = 0, j = 0;
+        sort(begin(nums), end(nums));
+        nums.erase(unique(begin(nums), end(nums)), end(nums)); 
+        for (int M = nums.size(); j < M; ++j) {
+            if (nums[i] + n <= nums[j]) ++i;
         }
-        
-        int j = 0;
-        for (int i = 0; i < newNums.size(); i++) 
-        {
-            while (j < newNums.size() && newNums[j] < newNums[i] + n) 
-            {
-                j++;
-            }
-            
-            int count = j - i;
-            ans = min(ans, n - count);
-        }
-        
-        return ans;
+        return n - j + i;
     }
 };
