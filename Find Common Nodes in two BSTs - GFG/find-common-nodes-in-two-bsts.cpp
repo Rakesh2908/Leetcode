9cf76f41b5016a749ff6a7deb1c42cd675b9c3eb
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-10-14 10:27:05 +0530

    Added solution - LeetHub

---
+++ Find Common Nodes in two BSTs - GFG/find-common-nodes-in-two-bsts.cpp
@@ -0,0 +1,169 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the nodes that are common in both BST. 
+    void inOrderTraversalHelper(Node* node, std::vector<int>& result) 
+    {
+        if (node == nullptr)
+            return;
+
+        inOrderTraversalHelper(node->left, result);
+        result.push_back(node->data);
+        inOrderTraversalHelper(node->right, result);
+    }
+    
+    vector <int> findCommon(Node *root1, Node *root2)
+    {
+     //Your code here
+        vector<int> nums1;
+        inOrderTraversalHelper(root1, nums1);
+        
+        vector<int> nums2;
+        inOrderTraversalHelper(root2, nums2);
+        
+        vector<int> ans;
+        int size1 = nums1.size();
+        int size2 = nums2.size();
+        int i = 0, j = 0;
+    
+        while (i < size1 && j < size2) 
+        {
+            if (nums1[i] == nums2[j]) 
+            {
+                // Found a common element
+                if (ans.empty() || ans.back() != nums1[i]) 
+                {
+                    ans.push_back(nums1[i]);
+                }
+                
+                i++;
+                j++;
+            } 
+            
+            else if (nums1[i] < nums2[j]) 
+            {
+                i++;
+            } 
+            
+            else 
+            {
+                j++;
+            }
+        }
+    
+        return ans;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root1 = buildTree(s);
+
+        getline(cin,s);
+        Node* root2 = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.findCommon(root1, root2);
+        for (int i : res)
+            cout << i << " ";
+        cout<< endl;
+    }
+
+	return 1;
+}
+// } Driver Code Ends
