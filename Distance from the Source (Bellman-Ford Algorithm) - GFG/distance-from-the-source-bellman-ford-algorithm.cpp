2b78817dc397b5da787d4eb0d0244bf24a393775
Rakesh2908 <96054089+Rakesh2908@users.noreply.github.com>
2023-12-18 13:04:13 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -15,38 +15,40 @@ class Solution {
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) 
     {
         // Code here
-        vector<int> dist(V,1e8);
-        dist[S]=0;
+          // initialize distances from source to all vertices as infinity
+    vector<int> dist(V, 1e8);
+    dist[S] = 0;
         
-        for(int i=0;i<V-1;i++)
-        {
-            for(auto it: edges)
-            {
-                int u = it[0];
-                int v = it[1];
-                int wt = it[2];
+    // iterate over all edges V-1 times and update distances
+    for (int i = 0; i < V - 1; i++) {
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
                 
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v])
-                {
-                    dist[v]=dist[u]+wt;
-                }
+            // if there is a shorter path to v through u, update distance
+            if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                dist[v] = dist[u] + wt;
             }
         }
+    }
         
-            for(auto it: edges)
-            {
-                int u = it[0];
-                int v = it[1];
-                int wt = it[2];
+    // check for negative-weight cycles by iterating over all edges one more time
+    for (auto it : edges) {
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
                 
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v])
-                {
-                    return {-1};
-                }
-            }
-        return dist;
+        // if there is still a shorter path to v through u, there is a negative-weight cycle
+        if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+            return {-1}; // negative-weight cycle detected
+        }
+    }
+        
+    return dist; // return the distances from the source vertex to all other vertices
     }
 };
+
 
 
 //{ Driver Code Starts.
